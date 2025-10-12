009bcf1e6323b2384b238e71ef27f0d5fd04ff71
ankurgupta1436 <wwwankurgupta824@gmail.com>
2025-10-12 21:52:26 +0530

    modified

--- vect.cpp
+++ vect.cpp
@@ -6,6 +6,8 @@ int main()
     vector<int> myVec;
     cout<<"Is myVect Empty? "<<myVec.empty()<<endl;
     myVec = {1,2,3,4,5};
+    printf("%d %d %d", myVec[0],myVec[1],myVec[2]);
+    cout<<endl;
     cout<<"Is myVec Empyty now "<<myVec.empty()<<endl;
   
     return 0;
